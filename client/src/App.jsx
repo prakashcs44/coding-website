cdcec30531d54f406cf20968d5664c37bdc02cee
prakashcs44 <porkashbatarai@gmail.com>
2024-01-24 20:29:38 +0530

    removed dead imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import SolveProblem from './pages/problempage/SolveProblem'
-import Test from './resize-panel/Test'
-import CodeEditor from './components/code-editor/CodeEditor'
-import Editor from './components/code-editor/Editor'
 function App() {
  
   return (
